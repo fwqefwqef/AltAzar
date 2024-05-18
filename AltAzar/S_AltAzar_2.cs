e6f349d78700fba99975b8b741f5209de9aa82a2
warycoolio <warycoolioalt.alt@gmail.com>
2024-05-18 02:44:50 -0400

    Add files via upload

---
+++ AltAzar/S_AltAzar_2.cs
@@ -0,0 +1,19 @@
+﻿using GameDataEditor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AltAzar
+{
+    public class S_AltAzar_2 : Skill_Extended
+	{
+		public override void SkillUseSingle(Skill SkillD, List<BattleChar> Targets)
+		{
+			base.SkillUseSingle(SkillD, Targets);
+            AltAzar_Ex_0.SwordAdd(this.BChar, -1);
+            AltAzar_Ex_0.SwordAdd(this.BChar, 0);
+
+        }
+    }
+}
